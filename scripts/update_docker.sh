a63d688e99df11635c580cea184c2d3d05c7777e
theniwo <accounts.niwo@mailbox.org>
2020-06-26 17:49:31 +0200

    Auto commit

--- scripts/update_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker commit $(docker inspect --format='{{.ID}}' gobuntu) theniwo/gobuntu:latest && docker push theniwo/gobuntu:latest
-if [ $? -eq 0 ] ; then
-	logger -i -t gobuntu "Gobuntu has commited and automatically pushed to docker hub"
-	else
-	logger -i -t gobuntu "Gobuntu has failed to commit and automatically push to docker hub"
-
-fi
--- scripts/update_git.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "Adding all files"
-git add .
-echo "Committing"
-git commit --all -m "Auto commit"
-echo "pushing"
-git push origin master
-
